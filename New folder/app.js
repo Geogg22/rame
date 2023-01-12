2aac200b2e36641254e88753520bf6468e30de99
Geogg22 <120256488+Geogg22@users.noreply.github.com>
2023-01-12 20:02:26 +0400

    Add files via upload

---
+++ New folder/app.js
@@ -0,0 +1,41 @@
+let objects 
+{
+ 
+let active = (Boolean);
+let categories = (students);
+add = (strings);
+console.log(string);
+
+
+
+
+let students;
+let name = 'sttt';
+img = 'https://cdn-icons-png.flaticon.com/512/53/53163.png';
+number = 598321458;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
